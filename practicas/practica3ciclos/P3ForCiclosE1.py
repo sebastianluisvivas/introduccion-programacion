f51b8654b88c90d745b6fbecbaf5efe7c8ba73e2
sebastianluisvivas <sebastian14vivas@hotmail.com>
2022-09-06 18:23:18 -0300

    organizado P3ForCiclosE1 e iniciando ejercicios guardado

---
+++ practicas/practica3ciclos/P3ForCiclosE1.py
@@ -0,0 +1,3 @@
+# Ejercicio 1 
+# a) Hacer un programa que muestre, mediante un ciclo, los primeros 5 números naturales (1, 2, 3, 4 y 5).
+# b) Hacer un programa que permita al usuario elegir un número n y luego muestre los primeros n números naturales (1, 2, · · · , n).s
