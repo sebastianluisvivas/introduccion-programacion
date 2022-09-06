4ce774080a47cf66ca1cf846e3d7258612b29664
sebastianluisvivas <sebastian14vivas@hotmail.com>
2022-09-06 18:58:34 -0300

    ejercicio 20 sin hacer

--- practicas/practica3ciclos/P3ForCiclosE1.py
+++ practicas/practica3ciclos/P3ForCiclosE1.py
@@ -1,3 +1,41 @@
 # Ejercicio 1 
 # a) Hacer un programa que muestre, mediante un ciclo, los primeros 5 números naturales (1, 2, 3, 4 y 5).
 # b) Hacer un programa que permita al usuario elegir un número n y luego muestre los primeros n números naturales (1, 2, · · · , n).s
+
+
+                        # A
+# print("Este programa emitirá los primeros 5 números naturales:")
+# for i in range(1,6):
+#     print(i)
+
+
+                        # B
+
+# n=int(input("Elija un número: "))
+# for i in range(1,n):
+#     print(i)
+
+
+
+
+
+
+
+# Ejercicio 20 F ♣
+# a) Escribir un programa que permita al usuario elegir un número m y un n y muestre todos los pares de numeros que se pueden formar 
+# con los números que están entre ellos. Por ej. si el usuario ingresara 4 y 6, el programa deberá mostrar
+# 4 4
+# 4 5
+# 4 6
+# 5 4
+# 5 5
+# 5 6
+# 6 4
+# 6 5
+# 6 6
+# b) Cambiar el programa para que use sólo un ciclo en vez de dos
+
+# m=int(input("Elija un primer número: "))
+# n=int(input("Elija un segundo número: "))
+# for i in range(m,n):
+#     print(i)
