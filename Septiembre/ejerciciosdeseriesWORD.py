820b9ee919c47a0bab4bb58d25e342fc88331a65
sebastianluisvivas <sebastian14vivas@hotmail.com>
2022-09-13 20:10:56 -0300

    3 ejercicios de ejerciciosdeserioesWORD.py hechos

--- Septiembre/ejerciciosdeseriesWORD.py
+++ Septiembre/ejerciciosdeseriesWORD.py
@@ -34,15 +34,55 @@
 #En este caso se debe utilizar un while
 
 # Pido la cantidad de términos que será n e inicio el contador
-n=int(input("Ingrese la cantidad de términos"))
+# n=int(input("Ingrese la cantidad de términos: "))
+# suma=0
+
+# # Como estoy usando while, a la variable i la debo inicializar en i
+# i=1
+
+# while (suma<=n):
+#     suma=suma+1/i
+#     # Incremento la variable i en 1
+#     i=i+1
+# # Al salir del while imprimo la cantidad de términos que se necesitan antes de llegar a n
+# print("Los términos que se necesitan antes de llegar a n son",i-1)
+
+
+
+
+
+
+
+# #                                          \ ____________________ Ejercicio 2 ____________________ / 
+
+# Escribir un programa que calcule e imprima el resultado de la siguiente serie, teniendo en cuenta que n es un número introducido por teclado. Si n = 4 la serie sería: 
+
+# Primero analizar la serie e identificar un término general. Un i fácil es el de arriba porque va desde 1 a n. Abajo siempre es 2 elevado a i
+# Crear ciclo for desde el primer valor de i (1) hasta llegar a n, también revisar signos
+
+# n=int(input("Ingrese la cantidad de términos: "))
+# suma=0
+# for i in range(1,n+1):
+#     if(i%2==0):
+#         suma=suma-i/(2**i)
+#     else:
+#         suma=suma+i/(2**1)
+# print("La suma de los",n,"primeros términos es", suma)
+
+
+
+
+
+
+# #                                          \ ____________________ Ejercicio 3 ____________________ / 
+
+# Escribir un programa que calcule la siguiente serie, teniendo en cuenta que n es un número entero introducido por teclado. Si n=5  la serie sería:
+
+n=int(input("Ingrese la cantidad de términos: "))
 suma=0
-
-# Como estoy usando while, a la variable i la debo inicializar en i
-i=1
-
-while (suma<=n):
-    suma=suma+1/i
-    # Incremento la variable i en 1
-    i=i+1
-# Al salir del while imprimo la cantidad de términos que se necesitan antes de llegar a n
-print("Los términos que se necesitan antes de llegar a n son",i-1)
+for i in range(1,n+1):
+    if (i%2==0):
+        suma=suma-i/(3**(i-1))
+    else:
+        suma=suma+i/(3**(i-1))
+print("La suma de los",n,"primeros términos es",suma)
