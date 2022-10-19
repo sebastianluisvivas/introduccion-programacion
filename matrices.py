16eb1e8e9a7b6836395c9dc0acdbc2e4a5ae6e74
sebastianluisvivas <112786862+sebastianluisvivas@users.noreply.github.com>
2022-10-19 11:14:03 -0300

    Add files via upload

--- matrices.py
+++ matrices.py
@@ -1,6 +1,6 @@
 # Ejercicio 25 práctica 5 - Listas
 
-def controlV(ruta):
+"""def controlV(ruta):
     patentes=darPatentes(ruta)
     for patente in patentes:
         veloc = controlVelocidad(patente)
@@ -23,4 +23,27 @@ def esta (matriz,e):
         for elem in lista:
             if elem == e:
                 return True
-    return False
+    return False """
+
+
+matriz=[[16,10,10,8,2],[20,10,8,6],[6,6,6,4,8,30],[8,4,2,4],[10,6,8,14,12],[12,12,18,10,6]]
+
+
+def cuantas (e,matriz):
+    lista = []
+    lista.append()
+    contador=0
+    for lista in matriz:
+        for elemento in lista:
+            if elemento == e:
+                contador=contador+1
+        lista.append(contador)
+        contador=0
+    return lista
+print(cuantas(matriz,6))
+
+
+
+
+
+
