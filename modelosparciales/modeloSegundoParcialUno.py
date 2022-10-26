2a79d5cbf5908a2f2c12d90bf1fcbdd226b1da1e
sebastianluisvivas <112786862+sebastianluisvivas@users.noreply.github.com>
2022-10-25 23:17:04 -0300

    comienzo de archivo para practicar parcial

---
+++ modelosparciales/modeloSegundoParcialUno.py
@@ -0,0 +1,16 @@
+# link modelo uno: https://virtual.grado.ungs.edu.ar/moodle/pluginfile.php?file=%2F71943%2Fmod_resource%2Fcontent%2F1%2FRecuSegundoParcial2016_1S.pdf
+
+# _________ Ejercicio 1 ______________
+
+
+def primordial(n):
+    a = n % n
+    if a   
+
+    return 
+
+# | PP |
+# Pido el n que es el primordial
+n = int(input("Ingrese un número: "))
+
+primordial(n)
