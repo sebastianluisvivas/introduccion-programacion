81f7d3646aaa1f1f81ca6d4aec03491828a30f48
sebastianluisvivas <112786862+sebastianluisvivas@users.noreply.github.com>
2022-10-26 11:55:17 -0300

    clase del 26 octubre
    avance en ejercicio del banco y agregados ejercicio en clase de patentes aleatorias

--- modelosparciales/modeloSegundoParcialUno.py
+++ modelosparciales/modeloSegundoParcialUno.py
@@ -1,16 +1,101 @@
 # link modelo uno: https://virtual.grado.ungs.edu.ar/moodle/pluginfile.php?file=%2F71943%2Fmod_resource%2Fcontent%2F1%2FRecuSegundoParcial2016_1S.pdf
 
 # _________ Ejercicio 1 ______________
-
+"""
 
 def primordial(n):
-    a = n % n
-    if a   
 
-    return 
+
+    return True
 
 # | PP |
 # Pido el n que es el primordial
 n = int(input("Ingrese un número: "))
 
-primordial(n)
+primordial(n) """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# _________ Ejercicio 2 ______________
+
+#https://es.stackoverflow.com/questions/413893/c%C3%B3mo-crear-funci%C3%B3n-que-devuelva-los-%C3%BAltimos-4-d%C3%ADgitos-de-los-n%C3%BAmeros-de-una-lis
+"""
+listaUsuarios=["pepito16", "papasFritas", "clave2016"]
+listaClaves=[20160430, "Ruta8y202", "aBc123"]
+listaDNI=[32451671,38007376,36961155]
+
+
+#Solicitud de datos
+nombreUsuario=input("Ingrese su nombre de usuario: ")
+clave=int(input("Ingrese su clave: "))
+
+
+
+# Verificación de datos USUARIO y CLAVE
+
+if nombreUsuario in listaUsuarios:
+    if clave in listaClaves:
+        print("Los datos son correctos")
+        listaDNI_str = (str,listaDNI)
+        lista.append()
+    else:
+        print("Los datos no son correctos")
+else:
+    print("Los datos ingresados no son correctos")
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+# _____ Ejercicio en clase
+
+
+# Hacer una función que devuelva una LISTA  formada con 5 patentes ("LL NNN LL") en donde cada elemento de la patente se arme al azar y la patente resultante NO
+#debe repetirse
+
+import random
+
+def patentes ():
+    # letras a utilizar
+    listaLetras=['A','B','C','D','E','F','G']
+
+    # lista a guardar futuras patentes
+    listaPatentes=[]
+
+    # Generando letras y numeros al azar
+    letrasAleatorias = random.choice(listaLetras)
+
+    numerosAleatorios = random.randint(0,99)
+
+
+
+    #cadena.upper()
+
+
+# PP
+
+patentes()
+
+
